9de62ccc787b2ba928933ca005ba1983cbf659f2
SzyWoj <148621843+SzyWoj@users.noreply.github.com>
2024-03-25 21:00:25 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace constructors
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Object
+            Car Ford = new Car("Mustang", "Black", "1969");
+
+            Console.WriteLine(Ford.model + " " + Ford.color + " " + Ford.yearOfProfuction);
+            Ford.Honk();
+        }
+    }
+    class Car : Vehicle
+    {
+        public string model;
+        public string color;
+        public string yearOfProfuction;
+
+        //Constructor 
+        public Car(string modelName, string modelColor, string modelYearOfProduction)
+        {
+            model = modelName;
+            color = modelColor;
+            yearOfProfuction = modelYearOfProduction;
+
+        }
+    }
+
+    class Vehicle
+    {
+        public void Honk()
+        {
+            Console.WriteLine("Tuu tuuu");
+        }
+    }
+
+    
+ 
+
+    
+}
